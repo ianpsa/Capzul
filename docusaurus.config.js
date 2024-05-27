b2a53ca2af57c2bbef80214a4b87d3fb43030b4d
Ian Pereira Simão <ian.simao@sou.inteli.edu.br>
2024-05-27 19:20:33 -0300

    fix: build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ianpsa', // Usually your GitHub org/user name.
   projectName: 'Capzul', // Usually your repo name.
-  deploymentBranch: 'develop',
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
