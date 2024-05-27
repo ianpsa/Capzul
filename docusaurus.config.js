0ea35a9c8c41883c6864ccf736f8b45e69aa6f68
Ian Pereira Simão <ian.simao@sou.inteli.edu.br>
2024-05-27 18:59:16 -0300

    Atualização do readme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   url: 'https://ianpsa.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Capzul/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -39,13 +39,6 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: {
-          sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/ianpsa/Capzul',
-        },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
